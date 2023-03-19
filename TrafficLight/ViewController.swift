e9aced16b57775a38424984ad62fd5b80e17f3e8
Vladimir <smile_friend@mail.ru>
2023-03-19 18:25:19 +0300

    Setup ViewController

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var orangeLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+       
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changePropertiesOfLight()
+        startButton.layer.cornerRadius = 20
+        
+        
     }
-
-
+    
+   private func changePropertiesOfLight() {
+        redLight.alpha = 0.3
+        orangeLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = 60
+        orangeLight.layer.cornerRadius = 60
+        greenLight.layer.cornerRadius = 60
+        
+    }
+    
+    @IBAction func startButtonDidTapped() {
+        startButton.setTitle("Next", for: .normal)
+              
+        
+    }
 }
 
+ 
